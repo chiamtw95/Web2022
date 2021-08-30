f3f4f4ef3de167773bf83a474a2aeedd0cf99649
chiamtw95 <chiamtw95@gmail.com>
2021-08-30 11:11:50 +0800

    ex07 solved

--- Lab1/Lab1-07.php
+++ Lab1/Lab1-07.php
@@ -1,1 +1,7 @@
 <!-- 7. Modify your answer to Exercise 3 to use for() instead of while(). -->
+<?php
+    for($temp=-50; $temp<=50; $temp+=5){
+        $celsius = ($temp - 32) * (5/9);
+        printf("Fahrenheit:%d Celsius:%d <br>", $temp, $celsius);
+    }
+?>
