abf406317f2da17257b784aa72f842c2d45956a4
chiamtw95 <chiamtw95@gmail.com>
2021-08-30 11:10:04 +0800

    question 6 solved

--- Lab1/Lab1-06.php
+++ Lab1/Lab1-06.php
@@ -1,8 +1,19 @@
-<!-- 6. Use while() to print a table of Fahrenheit and Celsius temperature
-equivalents from –50 degrees F to 50 degrees F in 5-degree
-increments. On the Fahrenheit temperature scale, water freezes at
-32 degrees and boils at 212 degrees. On the Celsius scale, water
-freezes at 0 degrees and boils at 100 degrees. So, to convert from
-Fahrenheit to Celsius, you subtract 32 from the temperature,
-multiply by 5, and divide by 9. To convert from Celsius to Fahrenheit,
-you multiply by 9, divide by 5, and then add 32. -->
+<!-- 
+    6. Use while() to print a table of Fahrenheit and Celsius temperature
+    equivalents from –50 degrees F to 50 degrees F in 5-degree
+    increments. On the Fahrenheit temperature scale, water freezes at
+    32 degrees and boils at 212 degrees. On the Celsius scale, water
+    freezes at 0 degrees and boils at 100 degrees. So, to convert from
+    Fahrenheit to Celsius, you subtract 32 from the temperature,
+    multiply by 5, and divide by 9. To convert from Celsius to Fahrenheit,
+    you multiply by 9, divide by 5, and then add 32. 
+-->
+
+<?php
+    $temp = -50;
+    while($temp <= 50){
+        $celsius = ($temp - 32) * (5/9);
+        printf("Fahrenheit:%d Celsius:%d <br>", $temp, $celsius);
+        $temp += 5;
+    }
+?>
