4411de0824d576ae1791af1f939fe05d27163396
chiamtw95 <chiamtw95@gmail.com>
2021-08-30 10:45:18 +0800

    ex03 solved

--- Lab1/Lab1-03.php
+++ Lab1/Lab1-03.php
@@ -1,4 +1,13 @@
-<!-- 3. Write a PHP program that sets the variable $first_name to your first
-name and $last_name to your last name. Print out a string containing
-your first and last name separated by a space. Also print out the
-length of that string. -->
+<!-- 
+    3. Write a PHP program that sets the variable $first_name to your first
+    name and $last_name to your last name. Print out a string containing
+    your first and last name separated by a space. Also print out the
+    length of that string. 
+-->
+<?php
+    $first_name = "Tow Wang";
+    $last_name = "Chiam";
+    $len = strlen($first_name) + strlen($last_name);
+    
+    printf("%s %s <br> Name length: %d", $first_name, $last_name, $len)
+?>
